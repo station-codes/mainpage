111736a5cf9946a86c46c7b0d4ae1f62b910213c
Nicolas Duchoud <duchonic@bluewin.ch>
2019-07-23 20:45:10 +0200

    pug added

--- index.js
+++ index.js
@@ -5,8 +5,15 @@ var work = require('../workpage/work.js');
 
 // respond with "hello world" when a GET request is made to the homepage
 
+app.set('view engine', 'pug');
+
 app.get('/', function (req, res) {
-  res.send('hello world');
+  res.render('index', {
+    title:'hey title', 
+    message:'station.codes stuff',
+    text:'some text',
+    footer:'the footer',
+  });
 });
 
 app.listen(31898);
