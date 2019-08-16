55d942e47584024bb8cbf17dfc5b921f1e35b7de
Nicolas <duchonic@bluewin.ch>
2019-08-16 17:03:23 +0200

    jade update

--- index.js
+++ index.js
@@ -1,33 +1,24 @@
-var express = require('express');
+var express = require('express')
+var stylus = require('stylus')
+var nib = require('nib')
+
+
 var app = express();
 
-var work = require('./work.js');
-var learn = require('./learn.js');
-var teach = require('./teach.js');
-var game = require('./game.js');
-var sail = require('./sail');
+var work = require('./work.js')
+var learn = require('./learn.js')
+var teach = require('./teach.js')
+var game = require('./game.js')
+var sail = require('./sail')
 
-app.set('view engine', 'pug');
+app.set('view engine', 'jade')
+app.use(express.static(__dirname + '/public'))
+
 
 app.get('/', function (req, res) {
   res.render('index', {
     title:'station.codes', 
-    message:"From 2008 to 2018 i worked as a software engineer for microcontrollers.\
-             All the work i've done was in C. In all the projects there was never a\
-             question about the programming language. In 2018 I buyed a pycom embedded\
-             board that can be programmed in micropython. I got stuck by the\
-             simplicity of python and started to read some books. In December 2018\
-             i discovered advent of code and tried it with python.\
-             On reddit i followed the discussions about which language is best\
-             suited for the advent of code challenges. People with computer sciences\
-             degree argued about functional programming, lambdas, tree sorting ...\
-             I had no idea what they talked about, i wanted to know all about!\
-             Luckly at my work i had a programm to work on in C++. I discovered that\
-             C++ 98/11/17 were different and got the idea around the OOP paradigm.\
-             I started attending meetups in Zurich about C++/Agile/GNU.\
-             This page is the result after one year of private R&D around software\
-             in the year 2019. I want to realise different projects with C++17, Databases,\
-             nodejs, python. I will try to teach knowledge of C to friends.",
+    message:'',
     text:'my metrics',
     footer:'last update 2019-08-01',
   });
